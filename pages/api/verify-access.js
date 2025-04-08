5a84d270b10cbe9a9ccba7b7c83d2d3c3ff5f59e
aegidos <andi.schmid1210@gmail.com>
2025-04-08 20:40:41 +0200

    feat: added many collections to the allowlist

--- pages/api/verify-access.js
+++ pages/api/verify-access.js
@@ -17,7 +17,12 @@ export default async function handler(req, res) {
       '0x60e4d786628fea6478f785a6d7e704777c86a7c6', // MAYC
       '0xb3443b6bd585ba4118cae2bedb61c7ec4a8281df', // Gs on ape
       '0xfa1c20e0d4277b1e0b289dffadb5bd92fb8486aa', // NPC
-      '0x91417bd88af5071ccea8d3bf3af410660e356b06'  // zards
+      '0x91417bd88af5071ccea8d3bf3af410660e356b06',  // zards
+      '0xcf2e5437b2944def3fc72b0a7488e87467c7d76c',  // Froglings
+      '0xa096af26affe37cc2a56ecf381b754a20b6ddf20',  // OVISAURS
+      '0x35b70c728ce7bc2e2593100673a0ccd9ef4e1c7b',  // spunkys
+      '0xdd2da83d07603897b2eb80dc1f7a0b567ad1c2c6',  // Pixl Pals
+      '0x0178a9d0b0cba1b2ede3afdb6dd021db24ff4240'  // Forever Undead
     ];
 
     for (const contractAddress of contractAddresses) {
